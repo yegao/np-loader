b10e55a8b8e1f054d2aa4f1fbc37d31cedd1d8a3
yegao <familyink@163.com>
2018-05-02 17:43:14 +0800

    module.exports=

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports = function (source) {
-  var reg = new RegExp('<np>.*?</np>','g')
-  var res = source.replace(reg,'<div>-------------</div>')
-  return res;
+  var reg = new RegExp('<np>.*?</np>','g');
+  return "module.exports = " + JSON.stringify(source.replace(reg,'<div>-------------</div>'));
 }
