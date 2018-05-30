73d6177723c68e33a3d5e7e4d5245944bde3fbe5
yegao <familyink@163.com>
2018-05-30 19:31:56 +0800

    loader complete template

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 module.exports = function (source) {
-  var reg = new RegExp('<np>.*?</np>','g');
-  return "module.exports = " + JSON.stringify(source.replace(reg,'<div>-------------</div>'));
+  var template = /<template[\s\S]*?>[\s\S]*?<([a-zA-Z]*)[\s\S]*?>([\s\S]*?)<\/template>/g
+  var templates = template.exec(source);
+  const res = `var element = document.createElement('${templates[1]}');
+  element.innerHTML = \`${templates[2]}\`;
+  let component = niepan(element);`;
+  return `${res}module.exports = component;`;
 }
